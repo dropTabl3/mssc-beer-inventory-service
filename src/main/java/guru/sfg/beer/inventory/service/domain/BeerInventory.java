9d052f8fa0d74a728cee9e108116d353c4985145
Massimo Barbero <mxmo.barbero@gmail.com>
2023-10-08 21:06:52 +0200

    UUID/BLOB issue

--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -20,7 +20,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -42,7 +44,7 @@ public class BeerInventory extends BaseEntity{
         this.upc = upc;
         this.quantityOnHand = quantityOnHand;
     }
-
+    @Type(type="org.hibernate.type.UUIDCharType")
     private UUID beerId;
     private String upc;
     private Integer quantityOnHand = 0;
